99e94b25cf8607efded9a745bb7c6bde6ed52792
morenaku <boss.yomohiro@gmail.com>
2018-04-07 14:01:48 +0000

    Guard install

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,4 +6,18 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  
+  ENV['RAILS_ENV'] ||= 'test' 
+  require File.expand_path('../../config/environment', __FILE__) 
+  require 'rails/test_help' 
+  require "minitest/reporters" 
+  Minitest::Reporters.use!  
+  
+  class ActiveSupport::TestCase   
+    # Setup all fixtures in test/fixtures/*.yml for all tests   
+    # in alphabetical order.   
+    fixtures :all    
+    
+    # Add more helper methods to be used by all tests here... 
+    end
 end
